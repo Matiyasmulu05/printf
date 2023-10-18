d770cba3b66bacf23536c775ec36928bdf12360c
SYZBA1 <142875720+SYZBA1@users.noreply.github.com>
2023-10-17 19:14:10 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -3,13 +3,13 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
+ * print_char - Prints a char.
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
  * @flags:  Calculates active flags
  * @width: Width
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: Size specifier.
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
@@ -22,12 +22,12 @@ int print_char(va_list types, char buffer[],
 /************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
- * @types: List a of arguments
+ * @types: List a of arguments.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: Size specifier.
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
@@ -76,8 +76,8 @@ int print_string(va_list types, char buffer[],
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
- * print_percent - Prints a percent sign
- * @types: Lista of arguments
+ * print_percent - Prints a percent sign.
+ * @types: Lista of arguments.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
@@ -102,11 +102,11 @@ int print_percent(va_list types, char buffer[],
  * print_int - Print int
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @size: Size specifier.
+ * Return: Number of chars printed.
  */
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
