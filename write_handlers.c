183e75de97531f90657f9c6245a9adf5b52c8eb7
Matiyasmulu05 <140404220+Matiyasmulu05@users.noreply.github.com>
2023-10-18 05:13:30 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -5,10 +5,10 @@
  * handle_write_char - Prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
+ * @flags:  Calculates active flags
  * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
+ * @precision: precision specifier.
+ * @size: Size specifier.
  *
  * Return: Number of chars printed.
  */
